8c3b521d5d7b9c74677306da2a6d1f5fcb1bc091
Victoria <vika.derevcova@mail.ru>
2019-02-23 18:55:19 +0700

    Implement board output function in stdout

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "8 r n b q k b n r" << endl;
+    cout << "7 p p p p p p p p" << endl;
+    cout << "6" << endl << "5" << endl << "4" << endl << "3" << endl;
+    cout << "2 P P P P P P P P" << endl;
+    cout << "1 R N B Q K B N R" << endl;
+    cout << "  a b c d e f g h" << endl;
+    return 0;
+}
